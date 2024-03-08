a426a4cc92efac9032e794f89530c6d25064ae1a
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-03-08 11:52:04 -0600

    Added axios with API key & API url

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function getCityData(city) {
+  let apiKey = "73fd7aeeb1fc6do18b8423c70f3b718t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+}
+
 function showCityInput(event) {
   event.preventDefault();
   let selectedCity = document.querySelector("#city-input");
   let cityValue = document.querySelector("#city-value");
   cityValue.innerHTML = selectedCity.value;
+
+  getCityData(selectedCity.value);
 }
 
 let searchForm = document.querySelector("#search-form");
