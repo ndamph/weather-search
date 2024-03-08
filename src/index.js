1f956e93a819f02cfa04acfc8b47be1b133294fd
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-03-08 15:50:27 -0600

    Updated current conditions using the API

--- src/index.js
+++ src/index.js
@@ -2,8 +2,42 @@ function showCityData(response) {
   let temperatureElement = document.querySelector("#current-temp");
   let temperature = response.data.temperature.current;
   let cityValue = document.querySelector("#city-value");
+  let conditionElement = document.querySelector("#condition");
+  let condition = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  let wind = response.data.wind.speed;
+  let dateElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
   cityValue.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  conditionElement.innerHTML = condition;
+  humidityElement.innerHTML = humidity;
+  windElement.innerHTML = wind;
+  dateElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function getCityData(city) {
